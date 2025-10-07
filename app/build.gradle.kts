73b792323571306c0af5e6ad5b5b1165b57ff890
Cem Çakmak <alicemcakmak97@gmail.com>
2025-10-08 00:13:39 +0200

    Add haptic feedback to beverage type selection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.cemcakmak.hydrotracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "0.9.11-internal"
+        versionCode = 18
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
